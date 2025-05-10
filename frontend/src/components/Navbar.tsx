796f7b7e3a1ea0c63eb980709142aaa0d9700d69
fido1hn <confidoekeanya@gmail.com>
2025-05-10 03:48:56 +0100

    setup connect button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,29 +1,33 @@
+import { ConnectButton } from "@mysten/dapp-kit";
 import { useNavigation } from "../providers/navigation/NavigationContext";
 
 const Navbar = () => {
   const { currentPage, navigate } = useNavigation();
   return (
     <nav className="bg-gray-200 dark:bg-gray-800 p-4 shadow-md">
-      <ul className="flex space-x-6">
-        <li>
-          <button
-            onClick={() => navigate("/")}
-            className={`px-4 py-2 rounded ${
-              currentPage === "/" ? "bg-blue-700" : ""
-            }`}>
-            Home
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => navigate("/wallet")}
-            className={`px-4 py-2 rounded ${
-              currentPage === "/wallet" ? "bg-blue-700" : ""
-            }`}>
-            Wallet
-          </button>
-        </li>
-      </ul>
+      <div className="flex justify-between">
+        <ul className="flex space-x-6">
+          <li>
+            <button
+              onClick={() => navigate("/")}
+              className={`px-4 py-2 rounded ${
+                currentPage === "/" ? "bg-blue-700" : ""
+              }`}>
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => navigate("/wallet")}
+              className={`px-4 py-2 rounded ${
+                currentPage === "/wallet" ? "bg-blue-700" : ""
+              }`}>
+              Wallet
+            </button>
+          </li>
+        </ul>
+        <ConnectButton />
+      </div>
     </nav>
   );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "@mysten/dapp-kit/dist/index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./providers/theme/ThemeProvider.tsx";
 import NavigationProvider from "./providers/navigation/NavigationProvider.tsx";
